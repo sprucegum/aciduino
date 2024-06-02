2fea45d6ca78c56b850d9461866ddb85260edcaa
Jade Lacosse <jade.lacosse@stocksy.com>
2024-06-01 20:15:20 -0700

    base changes from v2.0.0

--- v2/AciduinoV2/AciduinoV2.ino
+++ v2/AciduinoV2/AciduinoV2.ino
@@ -1,6 +1,6 @@
 /*!
  *  @file       AciduinoV2.ino
- *  Project     Aciduino V2
+ *  Project     Aciduino V2 BeatPort
  *  @brief      Roland 303 and 808 step sequencer clone
  *  @version    2.0.0
  *  @author     Romulo Silva
@@ -46,9 +46,9 @@
 //#include "src/ports/avr/mega.h"
 //#include "src/ports/teensy/protoboard.h"
 //#include "src/ports/esp32/wroom.h"
-#include "src/ports/esp32/wroom-ext1.h"
+//#include "src/ports/esp32/wroom-ext1.h"
 //#include "src/ports/teensy/uone.h"
-//#include "src/ports/avr/midilab_mega.h"
+#include "src/ports/avr/midilab_mega.h"
 
 // globals
 bool _playing = false;
